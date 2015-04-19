c565cb6607d3d633009e416ab7c3e0ca5058f40a
patmc2go <patmc@us.ibm.com>
2015-04-19 11:05:20 -0400

    point to alt Driver location; http

--- bin/common.sh
+++ bin/common.sh
@@ -34,7 +34,7 @@ install_db2_odbc() {
 		DB2_DIR="$1"
 		if [ ! -d "$DB2_DIR/clidriver" ]; then
 		        #mkdir -p "$DB2_DIR"
-			DB2_DSDRIVER_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/linuxx64_odbc_cli.tar.gz"
+			DB2_DSDRIVER_URL="http://rtpgsa.ibm.com/home/p/a/patmc/public/v9.7fp9_linuxx64_odbc_cli.tar.gz"
 			status "downloading DB2 ODBC driver..."
 			curl ${DB2_DSDRIVER_URL} -s -o ${DB2_DIR}/clidriver.tgz
 			tar xzf ${DB2_DIR}/clidriver.tgz -C ${DB2_DIR}
