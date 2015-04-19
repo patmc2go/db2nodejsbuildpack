84563c73e03acd0dcef8c50436c88b2e46cf85c6
patmc2go <patmc2go@users.noreply.github.com>
2015-04-19 11:53:37 -0400

    point to alt Driver location; http; added echo alerts

--- bin/common.sh
+++ bin/common.sh
@@ -32,9 +32,15 @@ cat_npm_debug_log() {
 
 install_db2_odbc() {
 		DB2_DIR="$1"
+		echo "---------------------------------"
+		echo "patmc2go in use"
+		echo $DB2_DIR
+		echo "---------------------------------"
 		if [ ! -d "$DB2_DIR/clidriver" ]; then
 		        #mkdir -p "$DB2_DIR"
 			DB2_DSDRIVER_URL="http://rtpgsa.ibm.com/home/p/a/patmc/public/v9.7fp9_linuxx64_odbc_cli.tar.gz"
+			echo "---------------------------------"
+			echo "DB2_DSDRIVER_URL:" ${DB2_DSDRIVER_URL}
 			status "downloading DB2 ODBC driver..."
 			curl ${DB2_DSDRIVER_URL} -s -o ${DB2_DIR}/clidriver.tgz
 			tar xzf ${DB2_DIR}/clidriver.tgz -C ${DB2_DIR}
@@ -42,4 +48,5 @@ install_db2_odbc() {
                         rm -rf ${DB2_DIR}/clidriver.tgz
 		fi
 		export IBM_DB_HOME="$DB2_DIR/clidriver"
+		echo "IBM_DB_HOME:"" $IBM_DB_HOME
 }
